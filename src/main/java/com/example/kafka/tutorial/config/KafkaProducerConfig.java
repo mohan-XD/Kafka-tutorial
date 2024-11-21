8b1e0c588bee782840d047df7aaa8028279e691c
Mohan Anuradha <mohan.at145@gmail.com>
2024-11-22 03:52:57 +0530

    Initial Spring boot project commit

--- src/main/java/com/example/kafka/tutorial/config/KafkaProducerConfig.java
+++ src/main/java/com/example/kafka/tutorial/config/KafkaProducerConfig.java
@@ -1,6 +1,8 @@
 package com.example.kafka.tutorial.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -20,9 +22,9 @@ public class KafkaProducerConfig {
 
     private Map<String, Object> producerProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("key.serializer", StringSerializer.class);
-        props.put("value.serializer", StringSerializer.class);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
     }
 
